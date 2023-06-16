f9aad46cafce95c8be94037750d31ae773403079
master <spikerus93@rambler.ru>
2023-06-16 20:01:06 +0300

    Решение домашего задания по 2-ому семинару

---
+++ task10/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8 
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int thirdDigit = Convert.ToInt32(Console.ReadLine());
+string Number = Convert.ToString(thirdDigit);
+Console.WriteLine("вторая цифра этого числа -> "+Number[1]);
---
+++ task13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string anyNumber = Convert.ToString(Number);
+if (anyNumber.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumber[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
